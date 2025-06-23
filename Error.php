f59836f1b0257ccb3cf835d9b8ac938aaf814a6f
Pavel Kácha <kacha@shoptet.cz>
2025-06-23 16:53:31 +0200

    Add PHP8.4 compatibility

--- Error.php
+++ Error.php
@@ -4,7 +4,7 @@ namespace Twisto;
 class Error extends \Exception {
     public $data;
 
-    public function __construct($message="", $data=null, $code = 0, \Exception $previous = null)
+    public function __construct($message="", $data=null, $code = 0, ?\Exception $previous = null)
     {
         $this->data = $data;
         parent::__construct($message, $code, $previous);
